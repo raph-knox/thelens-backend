54ed0c26cb884707d813cbbe3ec1847ccf8762ce
Raphael <raphaelositaomeje@gmail.com>
2025-07-24 15:26:34 +0100

    Add home route to root URL

--- thelens/urls.py
+++ thelens/urls.py
@@ -2,15 +2,20 @@ from django.contrib import admin
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from news.views import ArticleViewSet
+from django.http import HttpResponse
 
 router = DefaultRouter()
 router.register(r'articles', ArticleViewSet)
 
+def home(request):
+    return HttpResponse("Welcome to The Lens API")
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include(router.urls)),
     path('api/', include('news.urls')), 
+    path('', home),
+    path('api-token-auth/', obtain_auth_token),
 ]
 
 from django.conf import settings
