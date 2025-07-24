e33600dc3f95de72632f12e204603fd7ff0c3a92
Raphael <raphaelositaomeje@gmail.com>
2025-07-24 15:04:44 +0100

    Fix static files with WhiteNoise

--- thelens/settings.py
+++ thelens/settings.py
@@ -64,7 +64,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'django.middleware.security.SecurityMiddleware',
 ]
 
 ROOT_URLCONF = 'thelens.urls'
